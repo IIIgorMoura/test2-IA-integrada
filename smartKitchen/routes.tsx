c4551cbed8828ac5a22c2ddccc62b94438ac6a66
IIIgorMoura <133662971+IIIgorMoura@users.noreply.github.com>
2024-04-15 16:45:11 -0300

    add pages

--- smartKitchen/routes.tsx
+++ smartKitchen/routes.tsx
@@ -1,6 +1,11 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import { Home } from './pages/Index';
 import { Mecanico } from './pages/mecanico';
+import { Musica } from './pages/musica'
+import { RecomendacaoEstudos } from './pages/recomendacaoEstudos'
+import { Traducao } from './pages/traducao'
+
 import { Ionicons } from '@expo/vector-icons/';
 
 const Tab = createBottomTabNavigator();
@@ -25,6 +30,48 @@ export function Routes() {
             <Tab.Screen
                 name="mecanico"
                 component={Mecanico}
+                options={{
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return (<Ionicons size={25} color={"#000"} name="lock-closed" />)
+                        }
+                        return (<Ionicons size={20} color={"#000"} name="lock-closed-outline" />)
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="musica"
+                component={Musica}
+                options={{
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return (<Ionicons size={25} color={"#000"} name="lock-closed" />)
+                        }
+                        return (<Ionicons size={20} color={"#000"} name="lock-closed-outline" />)
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="recomendacaoEstudos"
+                component={RecomendacaoEstudos}
+                options={{
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return (<Ionicons size={25} color={"#000"} name="lock-closed" />)
+                        }
+                        return (<Ionicons size={20} color={"#000"} name="lock-closed-outline" />)
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="tradutor"
+                component={Traducao}
                 options={{
                     tabBarShowLabel: false,
                     headerShown: false,
